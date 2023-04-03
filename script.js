7476e6150306d8a839bf2effdd385c5184ffa6e9
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-04-03 20:13:57 +0700

    Create function to show toprated movies and now playing movies from the movie database api

--- script.js
+++ script.js
@@ -8,23 +8,62 @@ window.addEventListener("scroll", function () {
   }
 });
 
-btnUp.addEventListener("click", function(){
-    window.scrollTo(0, 0);
+btnUp.addEventListener("click", function () {
+  window.scrollTo(0, 0);
 });
 
 // Get DATA from API
-const apiKey = '';
-const endpointUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
+const apiKey = "";
+const topRatedMovies = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
+const nowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
 
-async function getTopRatedMovies() {
+async function getNowPlaying() {
   try {
-    const response = await fetch(endpointUrl);
-    const data = await response.json();
-    console.log(data.results[0]);
+    const response = await fetch(nowPlaying);
+    const movies = await response.json();
+
+    let data = "";
+    const movie = movies.results[0];
+    console.log(movie);
+
+    data =
+      data +
+      `
+      <img src="https://image.tmdb.org/t/p/original/${movie.backdrop_path}" alt="" />
+    `;
+
+    document.querySelector("#hero").innerHTML = data;
   } catch (error) {
     console.error(error);
   }
 }
 
-getTopRatedMovies();
+async function getTopRatedMovies() {
+  try {
+    const response = await fetch(topRatedMovies);
+    const movies = await response.json();
 
+    let data = "";
+    for (let i = 0; i < movies.results.length; i++) {
+      const movie = movies.results[i];
+
+      data =
+        data +
+        `
+        <div class="card">
+            <div class="card-item">
+              <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Image Movie" />
+              <h3>${movie.title}</h3>
+              <p>${movie.overview}</p>
+            </div>
+          </div>
+      `;
+    }
+    document.querySelector(".card-wrapper").innerHTML = data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getNowPlaying();
+getTopRatedMovies();
